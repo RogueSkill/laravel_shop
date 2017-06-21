296a5cb9cceef7370b3a668d05dd428dad84b234
wmm4376 <497801142@qq.com>
2017-06-21 22:34:13 +0800

    后台首页改

--- resources/views/admin/info.blade.php
+++ resources/views/admin/info.blade.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<link href="{{asset('./admin/css/bootstrap.min.css')}}" rel="stylesheet" />
+		<link href="{{asset('../admin/css/bootstrap.min.css')}}" rel="stylesheet" />
         <link rel="stylesheet" href="{{asset('../admin/css/style.css')}}"/>
         	<link rel="stylesheet" href="{{asset('../admin/css/ace.min.css')}}" />
         <link rel="stylesheet" href="{{asset('../admin/css/font-awesome.min.css')}}" />
