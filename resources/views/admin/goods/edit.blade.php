bb970f0e87aa34a57d3c522531a8b02f8343fa6d
charlesaiden <charlesaiden@sina.com>
2017-06-27 14:23:36 +0800

    修改商品添加bug 添加部分商品修改功能

--- resources/views/admin/goods/edit.blade.php
+++ resources/views/admin/goods/edit.blade.php
@@ -7,6 +7,7 @@
 <meta name="renderer" content="webkit">
 <title></title>
 <link rel="stylesheet" href="{{asset('style/css/pintuer.css')}}">
+<link rel="stylesheet" href="{{asset('style/css/bootstrap.min.css')}}">
 <link rel="stylesheet" href="{{asset('style/css/admin.css')}}">
 <script src="{{asset('style/js/jquery.js')}}"></script>
 <script src="{{asset('style/js/pintuer.js')}}"></script>
@@ -27,7 +28,7 @@
           <label>标题：</label>
         </div>
         <div class="field">
-          <input type="text" class="input w50" value="" name="title" data-validate="required:请输入标题" />
+          <input type="text" class="input w50" value="{{$goodsrow['goods_name']}}" name="title" data-validate="required:请输入标题" />
           <div class="tips"></div>
         </div>
       </div>
@@ -41,7 +42,7 @@
             <select name="cid" class="input w50">
               <option value="">请选择分类</option>
               @foreach($goodtypes as $val)     
-              <option value="{{$val['id']}}">{{$val['name']}}</option>
+              <option <?php echo $val['id']==$goodsrow['typeid']? 'selected':'' ?> value="{{$val['id']}}">{{$val['name']}}</option>
               @endforeach
             </select>
             <div class="tips"></div>
@@ -53,7 +54,7 @@
           <label>产品编号(sn)：</label>
         </div>
         <div class="field">
-          <input type="text" class="input w50" name="goods_sn" value="" />
+          <input type="text" class="input w50" name="goods_sn" value="{{$goodsrow['goods_sn']}}" />
         </div>
       </div>
 
@@ -62,7 +63,7 @@
           <label>商城价格：</label>
         </div>
         <div class="field">
-          <input type="text" class="input w50" name="shop_price" value="" />
+          <input type="text" class="input w50" name="shop_price" value="{{$goodsrow['shop_price']}}" />
         </div>
       </div>
 
@@ -71,7 +72,7 @@
           <label>市场价格：</label>
         </div>
         <div class="field">
-          <input type="text" class="input w50" name="mareket_price" value="" />
+          <input type="text" class="input w50" name="mareket_price" value="{{$goodsrow['mareket_price']}}" />
         </div>
       </div>
 
@@ -80,7 +81,7 @@
           <label>成本价格：</label>
         </div>
         <div class="field">
-          <input type="text" class="input w50" name="cost_price" value="" />
+          <input type="text" class="input w50" name="cost_price" value="{{$goodsrow['cost_price']}}" />
         </div>
       </div>
       
@@ -90,9 +91,22 @@
             <label>图片：</label>
           </div>
           <div class="field">
-            <input type="file" id="original_img" name="original_img[]" class="input tips" style="width:25%; float:left;"   data-toggle="hover" data-place="right" data-image="" />
+            @if($orimg)
+                @foreach($orimg as $val)
+
+                  <div class="col-xs-6 col-md-3">
+                    <input type="file" >
+                    <a href="#" class="thumbnail">
+                      <img width="100" height="100" src="../../upload/{{$val}}" />
+                    </a>
+                    <a href="#"><img width="15" height="15" src="../../images/icon/del.jpg" id="srcId"></a>
+                  </div>
+                <!-- <div><img src="../../upload/{{$val}}" /></div>    -->
+                @endforeach
+            @endif
+            <!-- <input type="file" id="original_img" name="original_img[]" class="input tips" style="width:25%; float:left;"   data-toggle="hover" data-place="right" data-image="" />
             <input type="button" class="button bg-blue margin-left" name="add" value="+"  style="float:left;">
-            <div class="tipss">图片尺寸：500*500</div>   
+            <div class="tipss">图片尺寸：500*500</div> -->   
           </div>
         </div>
       </div>
@@ -113,7 +127,7 @@
           <label>描述：</label>
         </div>
         <div class="field">
-          <script id="goods_remake" type="text/plain"  name="goods_remake" style="width:900px;height:200px;"></script>
+          <script id="goods_remake" type="text/plain"  name="goods_remake" style="width:900px;height:200px;">{{$goodsrow['goods_remake']}}</script>
           <!-- <textarea class="input" name="note" style=" height:90px;"></textarea> -->
           <div class="tips"></div>
         </div>
@@ -123,7 +137,7 @@
           <label>内容：</label>
         </div>
         <div class="field">
-        <script id="goods_content" type="text/plain"  name="goods_content" style="width:900px;height:300px;"></script>
+        <script id="goods_content" type="text/plain"  name="goods_content" style="width:900px;height:300px;">{{$goodsrow['goods_content']}}</script>
           <!-- <textarea name="content" class="input" style="height:450px; border:1px solid #ddd;"></textarea> -->
           <div class="tips"></div>
         </div>
@@ -131,39 +145,13 @@
      
       <div class="clear"></div>
 
-      <!-- <div class="form-group">
-        <div class="label">
-          <label>关键字标题：</label>
-        </div>
-        <div class="field">
-          <input type="text" class="input" name="s_title" value="" />
-        </div>
-      </div>
-
-      <div class="form-group">
-        <div class="label">
-          <label>内容关键字：</label>
-        </div>
-        <div class="field">
-          <input type="text" class="input" name="s_keywords" value=""/>
-        </div>
-      </div>
-
-      <div class="form-group">
-        <div class="label">
-          <label>关键字描述：</label>
-        </div>
-        <div class="field">
-          <textarea type="text" class="input" name="s_desc" style="height:80px;"></textarea>
-        </div>
-      </div> -->
 
       <div class="form-group">
         <div class="label">
           <label>销售数量：</label>
         </div>
         <div class="field">
-          <input type="text" class="input w50" name="sales_num" value="0"  data-validate="number:排序必须为数字" />
+          <input type="text" class="input w50" name="sales_num" value="{{$goodsrow['sales_num']}}"  data-validate="number:排序必须为数字" />
           <div class="tips"></div>
         </div>
       </div>
@@ -173,9 +161,9 @@
           <label>是否上架：</label>
         </div>
         <div class="field">
-          <input type="radio" class="" value="1" checked name="is_on_sale"/>是
+          <input type="radio" class="" <?php echo $goodsrow['is_on_sale']==1? 'checked':'' ?> value="1" name="is_on_sale"/>是
           <div class="tips"></div>
-          <input type="radio" class="" value="0" name="is_on_sale"/>否
+          <input type="radio" class="" <?php echo $goodsrow['is_on_sale']==0? 'checked':'' ?> value="0" name="is_on_sale"/>否
         </div>
       </div>
 
@@ -184,9 +172,9 @@
           <label>是否推荐：</label>
         </div>
         <div class="field">
-          <input type="radio" class="" checked name="is_recommend" value="1" />是
+          <input type="radio" class="" <?php echo $goodsrow['is_recommend']==1? 'checked':'' ?> name="is_recommend" value="1" />是
           <div class="tips"></div>
-          <input type="radio" class="" name="is_recommend" value="0" />否
+          <input type="radio" class="" <?php echo $goodsrow['is_recommend']==0? 'checked':'' ?> name="is_recommend" value="0" />否
         </div>
       </div>
 
@@ -195,9 +183,9 @@
           <label>是否新品：</label>
         </div>
         <div class="field">
-          <input type="radio" class="" checked name="is_new" value="1" />是
+          <input type="radio" class="" <?php echo $goodsrow['is_new']==1? 'checked':'' ?> name="is_new" value="1" />是
           <div class="tips"></div>
-          <input type="radio" class="" name="is_new" value="0" />否
+          <input type="radio" class="" <?php echo $goodsrow['is_new']==0? 'checked':'' ?> name="is_new" value="0" />否
         </div>
       </div>
 
@@ -206,9 +194,9 @@
           <label>是否热销：</label>
         </div>
         <div class="field">
-          <input type="radio" class="" checked name="is_hot" value="1" />是
+          <input type="radio" class="" <?php echo  $goodsrow['is_hot']==1? 'checked':'' ?> name="is_hot" value="1" />是
           <div class="tips"></div>
-          <input type="radio" class="" name="is_hot" value="0" />否
+          <input type="radio" class="" <?php echo $goodsrow['is_hot']==0? 'checked':'' ?> name="is_hot" value="0" />否
         </div>
       </div>
 
@@ -218,7 +206,7 @@
         </div>
         <div class="field"> 
           <script src="{{asset('js/datejs/laydate.dev.js')}}"></script>
-          <input type="text" class="laydate-icon input w50" id="datetime" name="datetime" onclick="laydate({istime: true, format: 'YYYY-MM-DD hh:mm:ss'})" value=""  data-validate="required:日期不能为空" style="padding:10px!important; height:auto!important;border:1px solid #ddd!important;" />
+          <input type="text" class="laydate-icon input w50" id="datetime" name="datetime" onclick="laydate({istime: true, format: 'YYYY-MM-DD hh:mm:ss'})" value="{{$goodsrow['updated_at']}}"  data-validate="required:日期不能为空" style="padding:10px!important; height:auto!important;border:1px solid #ddd!important;" />
           <div class="tips"></div>
         </div>
       </div>
@@ -227,7 +215,7 @@
           <label>库存：</label>
         </div>
         <div class="field">
-          <input type="number" class="input w50" name="store_count" value=""  />
+          <input type="number" class="input w50" name="store_count" value="{{$goodsrow['store_count']}}"  />
           <div class="tips"></div>
         </div>
       </div>
@@ -236,7 +224,7 @@
           <label>点击次数：</label>
         </div>
         <div class="field">
-          <input type="number" class="input w50" name="click_num" value="" data-validate="member:只能为数字"  />
+          <input type="number" class="input w50" name="click_num" value="{{$goodsrow['click_num']}}" data-validate="member:只能为数字"  />
           <div class="tips"></div>
         </div>
       </div>
@@ -254,6 +242,7 @@
 
 </body></html>
 <script>
+      $("#srcId").css({"position":"absolute","float":"right"});
       laydate({
             elem: '#datetime'
         });
@@ -275,6 +264,9 @@
      $(".upload_class").on('click','input[name=reduce]', function(){
           $(this).parent().parent().remove();
      });
+
+
+
 
   });
 </script>
