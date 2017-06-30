8833e5b0cfc42fdadea176515a61c244beb1eb33
wmm4376 <497801142@qq.com>
2017-06-30 19:03:02 +0800

    前台模板

--- app/Http/WebRoute.php
+++ app/Http/WebRoute.php
@@ -14,7 +14,9 @@ Route::get("login", "WebController@login");
 Route::get("register", "WebController@register");
 
 //商品详情页
-Route::get("goods/{goods_id}", "WebController@goods");
+// Route::get("goods/{goods_id}", "WebController@goods");
+Route::get("goods/{id}", "WebController@goods");
+
 
 //购物车页
 Route::get("cart", "WebController@cart");
